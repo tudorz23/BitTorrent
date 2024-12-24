6c158ad7b7a89d83260cb102f0a2ed6a5b402f61
Tudor Zaharia <zahariatm@yahoo.com>
2024-12-24 15:23:53 +0200

    Client: lock mutex only for wanted files.

--- src/Client.h
+++ src/Client.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <unordered_set>
 #include <pthread.h>
 
 #include "helper_objects.h"
@@ -17,7 +18,7 @@ class Client {
     pthread_mutex_t owned_files_mutex;
 
     std::unordered_map<std::string, std::vector<Segment>> owned_files;
-    std::vector<std::string> wanted_files;
+    std::unordered_set<std::string> wanted_files;
 
 
     Client(int numtasks, int rank);
@@ -32,34 +33,34 @@ class Client {
 
     void send_owned_files_to_tracker();
 
-    void receive_file_details_from_tracker(std::string &wanted_file, std::vector<int> &swarm,
+    void receive_file_details_from_tracker(const std::string &wanted_file, std::vector<int> &swarm,
                                            std::vector<Segment> &segments);
 
     void receive_file_swarm_from_tracker(std::vector<int> &swarm);
 
     void receive_file_segment_details_from_tracker(std::vector<Segment> &segments);
 
-    void update_swarm_from_tracker(std::string &wanted_file, std::vector<int> &swarm);
+    void update_swarm_from_tracker(const std::string &wanted_file, std::vector<int> &swarm);
 
-    int get_peer_with_min_load_for_segment(std::string &file, int segment_idx,
+    int get_peer_with_min_load_for_segment(const std::string &file, int segment_idx,
                                            std::vector<int> &swarm);
 
     void handle_has_segment_req_from_peer(int peer_idx);
 
     void handle_get_segment_req_from_peer(int peer_idx);
 
-    void announce_tracker_whole_file_received(std::string &file);
+    void announce_tracker_whole_file_received(const std::string &file);
 
-    void save_file(std::string &file);
+    void save_file(const std::string &file);
 
     void announce_tracker_all_files_received();
 
     // For debug.
     void print_files_after_read();
 
-    void print_swarm_for_file(std::string &file, std::vector<int> &swarm);
+    void print_swarm_for_file(const std::string &file, std::vector<int> &swarm);
 
-    void print_segment_details_for_file(std::string &file, std::vector<Segment> &segments);
+    void print_segment_details_for_file(const std::string &file, std::vector<Segment> &segments);
 };
 
 
